172db222c5b5017dc43ac8cacd0c009825fb7d5d
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2021-06-10 03:25:59 -0300

    first section finished

--- src/home/index.js
+++ src/home/index.js
@@ -7,15 +7,28 @@ function Home() {
     <div className="background-home">
       <section id="section-first" className="section-home">
         <div className="box-titles">
-          <h1>STARTOU MIZERAVI</h1>
+          <h4>GUILHERME PIMENTA</h4>
+          <h1>Front End Developer</h1>
+          <h3>
+            Together we will build modern interfaces for apps and websites.
+            <br />
+            Hands on and let's start to work.
+          </h3>
+
+          <a href="#section-skills" className="roll-to-icon">
+            <div className="mouse-icon">
+              <div className="mouse-roll"></div>
+            </div>
+          </a>
         </div>
+        <div className="gradient"></div>
         <video autoplay="true" muted="true" loop="true">
           <source src={background} type="video/mp4" />
         </video>
       </section>
 
-      <section className="">
-        <h1>STARTOU MIZERAVI</h1>
+      <section id="section-skills" className="section-home">
+        <h1>Skills</h1>
       </section>
     </div>
   );

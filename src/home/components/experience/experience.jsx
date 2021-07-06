6f4126c72bd28a625bc69f71735d9d7488fda558
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2021-07-06 15:12:37 -0300

    correcoes

--- src/home/components/experience/experience.jsx
+++ src/home/components/experience/experience.jsx
@@ -1,40 +1,40 @@
 import React from "react";
 // import "experience.scss";
 
-const projectsList = [
-  {
-    title: "UnB - Universidade de Brasília",
-    position: "Internship",
-    description:
-      "Hardware configuration and manutention. \nProcess management, data survey, document shipping and public attendance.",
-    startDate: "06/2018",
-    endDate: "12/2018",
-  },
-  {
-    title: "Anvisa - Agência Nacional de Vigilância Sanitária",
-    position: "Internship",
-    description: "",
-    startDate: "01/2019",
-    endDate: "08/2019",
-  },
-  {
-    title: "EBC - Empresa Brasil Comunicação",
-    position: "Web Developer",
-    description:
-      "Back-end Development - Node.js, Postgres, Api REST.\nFront-end - React, React Native.\nDesktop applications - Java 8, Maven, Docker. \nOS - Linux and Windows.Monitoring - Zabbix, Grafana. \nData analyze - Elasticsearch e PowerBI.",
-    startDate: "09/2019",
-    endDate: "10/2020",
-  },
-  {
-    title: "STF - Supremo Tribunal Federal",
-    position: "Front-end Developer",
-    description:
-      "Mobile first. \nVue 2 using Vuex , SASS, Bootstrap, grid-layout, components. \nApi Rest with axios.",
-    startDate: "11/2020",
-    endDate: "07/2021",
-  },
-  { title: "", position: "", description: "", startDate: "", endDate: "" },
-];
+// const projectsList = [
+//   {
+//     title: "UnB - Universidade de Brasília",
+//     position: "Internship",
+//     description:
+//       "Hardware configuration and manutention. \nProcess management, data survey, document shipping and public attendance.",
+//     startDate: "06/2018",
+//     endDate: "12/2018",
+//   },
+//   {
+//     title: "Anvisa - Agência Nacional de Vigilância Sanitária",
+//     position: "Internship",
+//     description: "",
+//     startDate: "01/2019",
+//     endDate: "08/2019",
+//   },
+//   {
+//     title: "EBC - Empresa Brasil Comunicação",
+//     position: "Web Developer",
+//     description:
+//       "Back-end Development - Node.js, Postgres, Api REST.\nFront-end - React, React Native.\nDesktop applications - Java 8, Maven, Docker. \nOS - Linux and Windows.Monitoring - Zabbix, Grafana. \nData analyze - Elasticsearch e PowerBI.",
+//     startDate: "09/2019",
+//     endDate: "10/2020",
+//   },
+//   {
+//     title: "STF - Supremo Tribunal Federal",
+//     position: "Front-end Developer",
+//     description:
+//       "Mobile first. \nVue 2 using Vuex , SASS, Bootstrap, grid-layout, components. \nApi Rest with axios.",
+//     startDate: "11/2020",
+//     endDate: "07/2021",
+//   },
+//   { title: "", position: "", description: "", startDate: "", endDate: "" },
+// ];
 
 export function Experience() {
   return (

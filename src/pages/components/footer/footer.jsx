047879be24a8eea7df09f7aaee3021965ee33bf2
Guilherme Pimenta Rodrigues Pereira <guilherme.pereira@ebc.com.br>
2021-11-07 15:28:45 -0300

    teste 2

--- src/pages/components/footer/footer.jsx
+++ src/pages/components/footer/footer.jsx
@@ -1,14 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './footer.scss';
 
 export default function Footer() {
   const API_TOKEN = process.env.REACT_APP_EMAIL_API_TOKEN;
-  useEffect(() => {
-    console.log(API_TOKEN);
-  }, []);
 
   return (
     <footer>
+      <p>{API_TOKEN}</p>
       <h3>© 2020-2021 Guilherme Pimenta All Rights Reserved</h3>
     </footer>
   );
